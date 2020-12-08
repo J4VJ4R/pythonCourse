ac7eb3c18fc50c332e53e541289895401e492c18
Javier <ingenierodesoftware73@gmail>
2020-12-08 19:24:11 +0000

    creata an if an array and find an elemente in this array

---
+++ conditionals/if.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+uno = 15
+dos = 15
+
+if uno < dos:
+    print ("is menor")
+elif uno == dos:
+    print ("Vamos al mar")
+else:
+    print ("es more big")
---
+++ conditionals/ifarray.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+house1 = ['sr', 'ms', 'young', 'villain']
+
+if 'villain' in house1:
+    print ("we found villain")
