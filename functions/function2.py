c8fbb1afe477b73799c2bbb7cf0d478cfacb7fc4
Javier <ingenierodesoftware73@gmail>
2020-12-08 20:28:55 +0000

    add tuplas and other functions

---
+++ functions/function2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def sum(num1, num2):
+    result = num1 + num2
+    return result
+
+print (sum(1, 3))
---
+++ tuplas/tupla1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+fruitsalat = ('apple', 'pineapple', 'banana', 5000)
+
+print(fruitsalat)
+
